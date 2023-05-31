cdffab5db03af6f74e8e15f387f18a87ce617300
Mohd Zia <ansarizia9335@gmail.com>
2023-06-01 01:25:11 +0530

    Modified home page

--- server/models/database.js
+++ server/models/database.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose');
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect(process.env.MONGODB_URI).then(() => {
-//   console.log("connection succesful");
-// }).catch((err) => {
-//   console.log("NO Connection"+ err);
-// });
-// console.log(process.env.MONGODB_URI);
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function(){
-//   console.log('Connected')
-// });
+
 const connect= async()=>{
  try{
   await mongoose.connect(process.env.MONGODB_URI);
